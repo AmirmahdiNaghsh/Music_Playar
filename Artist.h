56e0da8b69ccb99bbc603faa85f6eae54ae1d7e3
AmirmahdiNaghsh <am.naghsh@gmail.com>
2025-05-24 17:15:07 +0330

    Add SetName

--- Artist.h
+++ Artist.h
@@ -19,37 +19,9 @@ private:
 public:
     Artist() {}
     Artist(string n) : name(n) {}
-
+    
     void setName(const string& newName) {
         name = newName;
     }
-
-    void setAlbumCount(int count) {
-        albumCount = count;
-    }
-
-    void display() const {
-        cout << "Esm Chah Chah Zan: " << name << endl;
-        cout << "Album ha: " << albumCount << endl;
-        cout << "Ahang haye montasher shodeh: " << songcount << endl;
-        for (int i = 0; i < songcount; ++i) {
-            songs[i].display();
-        }
-    }
-
-    string getName() const {
-        return name;
-    }
-
-    void addSong(const Song& song) {
-        if (songcount < MAX_SONGS) {
-            songs[songcount++] = song;
-        }
-    }
-
-    void addPlaylist(const Playlist& playlist) {
-        if (playlistcount < MAX_PLAYLISTS) {
-            playlists[playlistcount++] = playlist;
-        }
-    }
+   
 };
